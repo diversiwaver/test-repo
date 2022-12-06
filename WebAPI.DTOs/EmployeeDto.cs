cfdf71748c845a8fa0b58d7ae99b43f5d3ab7d59
WrexBG <40688355+WrexBG@users.noreply.github.com>
2022-12-06 12:48:20 +0100

    Update EmployeeDto.cs
    test commit

--- WebAPI.DTOs/EmployeeDto.cs
+++ WebAPI.DTOs/EmployeeDto.cs
@@ -1,19 +1,18 @@
 ﻿using System;
 
-namespace WebAPI.DTOs
+namespace WebAPI.DTOs;
+
+public class EmployeeDto
 {
-	public class EmployeeDto
-	{
-		public int Id { get; set; }
-		
-		public string FirstName { get; set; }
-		public string MiddleName { get; set; }
+	public int Id { get; set; }
+	
+	public string FirstName { get; set; }
+	public string MiddleName { get; set; }
 
-		public string LastName { get; set; }
+	public string LastName { get; set; }
 
-		public DateTime BirthDate { get; set; }
+	public DateTime BirthDate { get; set; }
 
-		public int OfficeId { get; set; }
-	}
+	public int OfficeId { get; set; }
 }
 
